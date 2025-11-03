6f0242be630751a804b704987c6bce22d2575f04
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2025-11-03 09:56:44 -0500

    completed gettotalscore

--- from-scratch.js
+++ from-scratch.js
@@ -78,9 +78,10 @@ const getActiveUsersUnder30 = (users) => {
 console.log(getActiveUsersUnder30(sampleUsers))
 // Problem 9
 const getTotalScore = (users) => {
+  return users.reduce((acc,curr)=>acc+curr.score,0)
 
 };
-
+console.log(getTotalScore(sampleUsers))
 // Problem 10
 const sortUsersByScoreDescending = (users) => {
 
